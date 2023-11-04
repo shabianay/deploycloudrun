a963a6e8e0046fcfa02a138fc54c275c772ec015
Shabian Arsyl Yonanta <72251541+shabianay@users.noreply.github.com>
2023-11-04 12:38:02 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Portofolio | Shabian Arsyl Yonanta</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">PORTOFOLIO</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#portfolio">Portofolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">Tentang Saya</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <!-- Header -->
+  <header class="jumbotron jumbotron-fluid text-center bg-primary text-white">
+    <div class="container">
+      <h1 class="display-4">Selamat Datang di Portofolio UI/UX</h1>
+    </div>
+  </header>
+
+  <!-- Portofolio Section -->
+  <section id="portfolio" class="py-5">
+    <div class="container px-4 py-3">
+      <h2 class="text-center">Portofolio Saya</h2>
+      <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+        <div class="col-md-4">
+          <div class="card mb-4">
+            <img src="https://cdn.dribbble.com/users/11464607/screenshots/20388613/media/490e577ecc400bbcab0b22b02104dcb3.png?resize=1000x750&vertical=center" class="card-img-top" alt="Gambar 1">
+            <div class="card-body">
+              <h5 class="card-title">Seroom.id</h5>
+              <p class="card-text"><a href="https://dribbble.com/shots/20388613-Seroom-id">Lihat selengkapnya</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4">
+            <img src="https://cdn.dribbble.com/userupload/8887143/file/original-72a955c33b6283ed9c7ff376b4910860.png?resize=1000x750" class="card-img-top" alt="Gambar 2">
+            <div class="card-body">
+              <h5 class="card-title">ToothScan</h5>
+              <p class="card-text"><a href="https://dribbble.com/shots/22117006-ToothScan">Lihat selengkapnya</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4">
+            <img src="https://cdn.dribbble.com/users/11464607/screenshots/20387582/media/a8c6b6f68feee687151bcb99ee503437.png?resize=1000x750&vertical=center" class="card-img-top" alt="Gambar 3">
+            <div class="card-body">
+              <h5 class="card-title">Aplikasi Food Delivery</h5>
+              <p class="card-text"><a href="https://dribbble.com/shots/20387582-Aplikasi-Food-Delivery">Lihat selengkapnya</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- About Section -->
+  <section id="about" class="py-5">
+    <div class="container px-4 py-3">
+      <h2 class="text-center">Tentang Saya</h2>
+      <div class="row row g-4 py-5 row-cols-1 row-cols-lg-2">
+        <div class="col-md-6">
+          <img src="./assets/images/profil.jpg" class="rounded-circle w-75" alt="profil">
+        </div>
+        <div class="col-md-6">
+          <h3>Shabian Arsyl Yonanta</h3>
+          <p>
+            Saya seorang mahasiswa Manajemen Informatika tahun keempat di Universitas Negeri Surabaya yang memiliki rasa ingin tahu tinggi, komunikatif, dan juga orang yang mudah beradaptasi, dengan minat besar pada: Desain UI/UX, Pengembang Situs Website Front End & Back End, Pembuat Konten, Manajemen Media Sosial, Pembuatan dan Editing Video & Foto.
+          </p>
+          <p>
+            Hubungi saya jika ingin berkontak: <a href="mailto:email@contoh.com">shabianarsyl@gmail.com</a>.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="text-center py-3">
+    <p>&copy; 2023 <a href="https://www.instagram.com/shabianarsyl/">Shabianarsyl</a></p>
+  </footer>
+
+  <!-- Script Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
